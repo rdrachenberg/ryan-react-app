e0fce4cd67722dff8d86688808d0cdd13d135028
Ryan Drachenberg <tssinvestments@gmail.com>
2020-11-14 17:35:40 -0500

    heroku depoloyment

--- api/config/express.js
+++ api/config/express.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
 
     app.use(function(req, res, next) {
         // Website to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
         // Request methods to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         // Request headers to allow
