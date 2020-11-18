97006bcb51650bfc2630af5f886fa52fda0e2fde
Ryan Drachenberg <tssinvestments@gmail.com>
2020-11-17 22:16:46 -0500

    changed response header

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ dbConnection().then(() => {
             res.sendFile(path.join(__dirname + '/client/build/index.html'));
         });
     }
+     app.use(express.static(path.join(__dirname, 'client/build')));
+        app.use(express.static(path.join(__dirname, 'client/public')));
+        
+        // app.use(express.static(path.join(__dirname, '/api')));
+         // Handle React routing, return all requests to React app
+        app.get('*', function(req, res) {
+            res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        });
     app.listen(config.port, function () {
     console.log(`🌎  ==> API Server now listening on port ${config.port}!  🌎`);
     });
