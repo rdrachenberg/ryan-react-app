99354596c44942693d39fda6358d15a91dbaab78
Ryan Drachenberg <tssinvestments@gmail.com>
2020-11-18 14:46:46 -0500

    adjusted files for deployment

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,5 @@
 const router = require('../routes');
 
-const path = require('path');
-
 module.exports = (app) => {
     app.use('/api/message', router.message);
     app.use('/api/post', router.post);
@@ -10,8 +8,8 @@ module.exports = (app) => {
     
 
     // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
+    // app.get('*', function(req, res) {
+    //     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    // });
     // app.use('*', (req, res, next) => res.send('<h1>You know, somehting went wrong :thumbsdown: </h1>'));
 };
--- server.js
+++ server.js
@@ -19,20 +19,13 @@ dbConnection().then(() => {
     if(process.env.NODE_ENV === 'production'){
         app.use(express.static(path.join(__dirname, 'client/build')));
         app.use(express.static(path.join(__dirname, 'client/public')));
+        app.use(express.static('client/build'))
 
          // Handle React routing, return all requests to React app
         app.get('*', function(req, res) {
             res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
         });
     }
-    
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.use(express.static(path.join(__dirname, 'client/public')));
-
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-    });
 
     app.listen(config.port, function () {
     console.log(`🌎  ==> API Server now listening on port ${config.port}!  🌎`);
