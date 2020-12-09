e766bc9d3ecd9abc98fbc4f8fdb7afb4dc6298ba
Ryan Drachenberg <tssinvestments@gmail.com>
2020-12-09 14:02:15 -0500

    added link to cert homepage

--- client/src/Components/HomeContent/HomeContent.js
+++ client/src/Components/HomeContent/HomeContent.js
@@ -161,6 +161,8 @@ const HomeContent = () => {
                 </i>
                 <div className="col-md-9">
                     <p>Currently enrolled in Kingsland University Blockchain Developer program. I have completed the 9 month full stack developer portion of the program and am now in the final Blockchain Certification portion of it. I should have this completed in by next quarter. More information can be found <a href="https://kingslanduniversity.com/zero-to-blockchain-developer-program/" target="_blank" rel="noreferrer">here</a></p>
+                    <br/>
+                    <p><a href="https://blockchaincertificate-verify.kingsland.io/certificate/5fcdf04e686adb11255ebbab" target="_blank" rel="noreferrer">Here is my full stack certificate, logged and verified on the Ethereum Blockchain.</a></p>
                 </div>
                 <br/>
                 <h4>University of Central Florida</h4>
