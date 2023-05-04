797b530457f44a47eb16a041ac852255ecde6df3
Ryan Drachenberg <tssinvestments@gmail.com>
2023-05-03 20:37:10 -0400

    fixed broken linked images

--- client/src/Components/HomeContent/HomeContent.js
+++ client/src/Components/HomeContent/HomeContent.js
@@ -245,7 +245,7 @@ const HomeContent = () => {
                                 <li>Solidity and Smart Contracts<a href="https://blockchaincertificate-verify.kingsland.io/certificate/61bb0584815b0057d5c91cf1" target= "__blank"> here</a></li>
                             </ul>
                             <a href="https://blockchaincertificate-verify.kingsland.io/certificate/62fcb6b61918c47cc68be655">
-                                <img src="https://app.infura.io/loginhttps://gateway.ipfs.io/ipfs/Qmecvbieu6a7kBrPqo7oSYNy41hhiiVDUUCZKEuELknvNi" alt="Ryan Drachenberg Blockchain Final Projects Certification" width={'100%'} rounded/>
+                                <img src="/rd-blockchain-cert.jpg" alt="Ryan Drachenberg Blockchain Final Projects Certification" width={'100%'} rounded/>
                             </a>
 
                         </h5>
