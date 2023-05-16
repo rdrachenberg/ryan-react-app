673cc253e8ca6877fe35c43d41872f07087f30a1
Ryan Drachenberg <tssinvestments@gmail.com>
2023-05-16 13:23:25 -0400

    fixed broken img link

--- client/src/Components/MostLanguages/MostLanguages.js
+++ client/src/Components/MostLanguages/MostLanguages.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MostLanguages = () => {
     return (
         <div >
-            <img src='https://github-readme-stats.vercel.app/api/top-langs/?username=rdrachenberg&langs_count=5' alt='github most languages' id='github-most-languages'/>
+            <img src='https://github-readme-stats-sigma-five.vercel.app/api/top-langs/?username=rdrachenberg&langs_count=5' alt='github most languages' id='github-most-languages'/>
         </div>
     )
 }
